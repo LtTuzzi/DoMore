4670e8505db2ed48f104688ac859c58ef4032e15
ronewav <Ronewavhulahani123@gmail.com>
2025-11-11 04:12:53 +0200

    Added welcome text and Scanner to recieve inputs.

--- DoMore.java
+++ DoMore.java
@@ -1,3 +1,13 @@
+import java.util.Scanner;
+
 public class DoMore {
     
+    public static void main(String[] args){
+        
+        System.out.println("==========DoMore==========");
+        System.out.println("What's on your mind?");
+        System.out.println("(1,2,3)\n1.Add\n2.Remove\n3.Update");
+
+        Scanner scanner = new Scanner(System.in);
+    }
 }
